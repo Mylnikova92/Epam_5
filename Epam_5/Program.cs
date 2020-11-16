c9ba8a523203dac064e892c2f2095407db4e27b8
Daria <rabota3dmax@list.ru>
2020-11-16 21:21:48 +0300

    1

--- Epam_5/Program.cs
+++ Epam_5/Program.cs
@@ -6,7 +6,31 @@ namespace Epam_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+                int n;
+                Console.Write("Введите число элементов массива: ");
+                n = int.Parse(Console.ReadLine());
+                int[] arr = new int[n];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write("Введите arr[{0}] ", i);
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+                Console.ReadLine();
+
+
+          
+
+
+            Console.WriteLine("Элементы массива:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("arr[{0}] = {1}",
+                i, arr[i]);
+            }
+            Console.ReadLine();
+
         }
     }
 }
