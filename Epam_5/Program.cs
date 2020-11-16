80f22c689b4eea757acc16a5933388de9eb4a65b
Daria <rabota3dmax@list.ru>
2020-11-16 21:45:16 +0300

    готовый массив плюс пузырьковая сортировка

--- Epam_5/Program.cs
+++ Epam_5/Program.cs
@@ -18,8 +18,20 @@ namespace Epam_5
                 }
                 Console.ReadLine();
 
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
 
-          
 
 
             Console.WriteLine("Элементы массива:");
